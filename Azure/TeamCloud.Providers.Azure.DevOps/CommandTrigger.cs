145f61cb1ea41e1455f3f44761747b39a2a5a27a
Colby Williams <ColbyLWilliams@gmail.com>
2020-01-21 12:57:16 -0500

    initial proj and sln for Azure providers

---
+++ Azure/TeamCloud.Providers.Azure.DevOps/CommandTrigger.cs
@@ -0,0 +1,34 @@
+/**
+ *  Copyright (c) Microsoft Corporation.
+ *  Licensed under the MIT License.
+ */
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.DurableTask;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+namespace TeamCloud.Providers.Azure.DevOps
+{
+    public class CommandTrigger
+    {
+        [FunctionName(nameof(CommandTrigger))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = "command")] HttpRequest httpRequest,
+            [DurableClient] IDurableClient durableClient,
+            ILogger logger)
+        {
+            if (httpRequest is null)
+                throw new ArgumentNullException(nameof(httpRequest));
+
+            if (durableClient is null)
+                throw new ArgumentNullException(nameof(durableClient));
+
+            throw new NotImplementedException();
+        }
+    }
+}
