10c98df0f5da1d1b5e595bc6e21d76e4fbcbb634
Markus Heiliger <markus.heiliger@microsoft.com>
2020-04-23 23:17:35 -0700

    package upgrade on 0.1.354-pre

--- Azure/TeamCloud.Providers.Azure.bat
+++ Azure/TeamCloud.Providers.Azure.bat
@@ -6,6 +6,6 @@ dotnet build -c Debug
 
 SET terminal=wt -d .\TeamCloud.Providers.Azure.AppInsights cmd /C "func host start --no-build --script-root bin/Debug/netcoreapp3.1" ;
 SET terminal=%terminal% split-pane -V -d .\TeamCloud.Providers.Azure.DevOps cmd /C "func host start --no-build --script-root bin/Debug/netcoreapp3.1" ;
-SET terminal=%terminal% split-pane -V -d .\TeamCloud.Providers.Azure.DevTestLabs cmd /C "func host start --no-build --script-root bin/Debug/netcoreapp3.1"
+SET terminal=%terminal% split-pane -H -d .\TeamCloud.Providers.Azure.DevTestLabs cmd /C "func host start --no-build --script-root bin/Debug/netcoreapp3.1"
 
 start "" /B %terminal%
