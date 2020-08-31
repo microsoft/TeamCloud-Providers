056d8f4dd207fd97e1d4287ab510839c7ea7ddce
Colby Williams <ColbyLWilliams@gmail.com>
2020-08-31 14:02:59 -0400

    add set method to core and qualifiers to links

--- Shared/TeamCloud.Providers.Core/Clients/ProjectLinkExtensions.cs
+++ Shared/TeamCloud.Providers.Core/Clients/ProjectLinkExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using TeamCloud.Model.Data;
@@ -7,13 +8,16 @@ namespace TeamCloud.Providers.Core.Clients
 {
     public static class ProjectLinkExtensions
     {
-        public static ProjectLink WithGeneratedId(this ProjectLink projectLink, string source)
+        public static ProjectLink WithGeneratedId(this ProjectLink projectLink, string source, params string[] qualifiers)
         {
             if (projectLink is null)
                 throw new ArgumentNullException(nameof(projectLink));
 
             if (string.IsNullOrWhiteSpace(source))
                 throw new ArgumentException($"'{nameof(source)}' cannot be null or whitespace", nameof(source));
+
+            if (qualifiers.Any())
+                source += "|" + string.Join('|', qualifiers);
 
             if (!Guid.TryParse(source, out Guid projectLinkId))
             {
