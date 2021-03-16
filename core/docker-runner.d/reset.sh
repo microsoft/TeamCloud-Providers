9dbf3c403b76aa81bd0a6b0231cfaeb37dbb7e18
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-16 16:32:44 -0700

    ...

--- core/docker-runner.d/reset.sh
+++ core/docker-runner.d/reset.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-DIR=$(dirname "$0")
-TSK="create.sh"
+function ResolveScript () {
 
-# check if the task file exists in the process dir
-SCRIPT="$(find $PWD -maxdepth 1 -iname "$TSK")"
+    # check if the task file exists in the process dir
+    SCRIPT="$(find $PWD -maxdepth 1 -iname "$1")"
 
-if [[ -z "$SCRIPT" ]]; then 
-    # check if the task file exists in the default runner dir
-    SCRIPT="$(find /docker-runner.d -maxdepth 1 -iname "$TSK")"
-fi
+    if [[ -z "$SCRIPT" ]]; then 
+        # check if the task file exists in the default runner dir
+        SCRIPT="$(find /docker-runner.d -maxdepth 1 -iname "$1")"
+    fi
+
+    echo "$SCRIPT"
+}
 
 # isolate task script execution in sub shell  
-( exec "$SCRIPT"; exit $? ) || exit $?
+( exec "$( ResolveScript 'delete.sh' )" && exec "$( ResolveScript 'create.sh' )"; exit $? ) || exit $?
--- terraform/docker-runner.d/delete.sh
+++ terraform/docker-runner.d/delete.sh
@@ -9,15 +9,12 @@ readonly ComponentStateFile="/mnt/storage/terraform.tfstate"
 ComponentTemplateFile="$(echo "$ComponentTemplateFolder/azuredeploy.tf" | sed 's/^file:\/\///g')"
 ComponentTemplateJson="$(cat $ComponentTemplateFile | hcl2json)"
 
-# echo "$ComponentTemplateJson"
+echo "$ComponentTemplateJson"
 
 trace "Initializing Terraform"
 terraform init -no-color
 
-# trace "Updating Terraform Plan"
-# terraform plan -no-color -refresh -state=$ComponentStateFile -var "ComponentResourceGroupName=$ComponentResourceGroup"
-
-trace "Applying Terraform Plan"
-terraform apply -no-color -auto-approve -state=$ComponentStateFile -var "ComponentResourceGroupName=$ComponentResourceGroup"
+trace "Destroying Terraform Plan"
+terraform destroy -no-color -auto-approve -state=$ComponentStateFile
 
 # tail -f /dev/null
