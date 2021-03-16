58ece1fcfa8554550e1a81bd595c1669584e032d
Markus Heiliger <markus.heiliger@microsoft.com>
2021-03-16 14:51:53 -0700

    ...

--- terraform/docker-runner.d/create.sh
+++ terraform/docker-runner.d/create.sh
@@ -3,6 +3,7 @@
 trace() {
     echo -e "\n>>> $@ ...\n"
 }
+readonly ComponentStateFile="/mnt/storage/terraform.tfstate"
 
 ComponentTemplateFile="$(echo "$ComponentTemplateFolder/azuredeploy.tf" | sed 's/^file:\/\///g')"
 ComponentTemplateJson="$(cat $ComponentTemplateFile | hcl2json)"
@@ -13,6 +14,6 @@ trace "Initializing Terraform"
 terraform init -no-color
 
 trace "Applying Terraform Plan"
-terraform apply -no-color -auto-approve -var "ComponentResourceGroupName=$ComponentResourceGroup"
+terraform apply -no-color -auto-approve -state=$ComponentStateFile -var "ComponentResourceGroupName=$ComponentResourceGroup"
 
 # tail -f /dev/null
