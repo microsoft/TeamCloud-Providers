15974eeee3539e82fac530fee1b328b8da4b5b60
Markus Heiliger <markus.heiliger@microsoft.com>
2022-01-26 02:00:05 +0100

    ...

--- core/docker-entrypoint.sh
+++ core/docker-entrypoint.sh
@@ -35,10 +35,8 @@ sed -i "s/server_name.*/server_name $TaskHost;/g" /etc/nginx/http.d/default.conf
 if [[ "$(echo $TaskHost | tr '[:upper:]' '[:lower:]')" != "localhost" ]]; then
 
     echo "Starting web server ..." \
-        && nginx \
-        || { error "Web server failed to host '$TaskHost'" && exit 1 }
-
-        # && nginx -q && timeout 300 bash -c -- "while true; do [ '200' == '$(curl -s -o /dev/null -I -L -w '%{http_code}' http://$TaskHost)' ] && break || sleep 5; done" \
+        && nginx -q
+        && sleep 5
 
     # acquire a ssl certificate to use for web access
     # as certbot is sometimes a little bit picky we
