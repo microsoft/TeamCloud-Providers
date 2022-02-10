4cd27da0df44ea11557200aec2e62ad59bb1ab3c
Markus Heiliger <markus.heiliger@microsoft.com>
2022-02-10 17:54:30 +0100

    wait for ws sidecar improvements

--- runner/core/docker-entrypoint.d/30-wait-for-webserver.sh
+++ runner/core/docker-entrypoint.d/30-wait-for-webserver.sh
@@ -2,7 +2,12 @@
 
 waitForWebServer() {
 	while true; do
-		[ "200" == "$(curl -s -o /dev/null -I -L -m 5 -f -w '%{http_code}' https://$TaskHost/_healthcheck)" ] && break 
+		STATUS_CODE="$(curl -s -o /dev/null -I -L -m 5 -f -w '%{http_code}' https://$TaskHost/_healthcheck --resolve $TaskHost:443:127.0.0.1)"
+		[ "200" == "$STATUS_CODE" ] && echo "Web server starts responding on internal interface" && break 
+	done 
+	while true; do
+		STATUS_CODE="$(curl -s -o /dev/null -I -L -m 5 -f -w '%{http_code}' https://$TaskHost/_healthcheck)"
+		[ "200" == "$STATUS_CODE" ] && echo "Web server starts responding on external interface" && break 
 	done 
 }
 
