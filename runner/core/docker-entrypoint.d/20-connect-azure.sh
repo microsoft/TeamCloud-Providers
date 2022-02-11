2135e36799251a4adf5d0db1081b8543e603fe72
Markus Heiliger <markus.heiliger@microsoft.com>
2022-02-11 17:40:46 +0100

    show user details when authenticated

--- runner/core/docker-entrypoint.d/20-connect-azure.sh
+++ runner/core/docker-entrypoint.d/20-connect-azure.sh
@@ -2,7 +2,7 @@
 
 waitForAzureConnection() {
 	while true; do
-		az login --identity --allow-no-subscriptions --only-show-errors --output none && {
+		az login --identity --allow-no-subscriptions --only-show-errors --output none 2> /dev/null && {
 			export ARM_USE_MSI=true
 			export ARM_MSI_ENDPOINT='http://169.254.169.254/metadata/identity/oauth2/token'
 			export ARM_SUBSCRIPTION_ID=$ComponentSubscription
@@ -16,6 +16,7 @@ export -f waitForAzureConnection
 [ ! -z "$ComponentSubscription" ] \
 	&& trace "Connecting Azure" \
 	&& timeout 300 bash -c "waitForAzureConnection" \
+	&& az ad signed-in-user show \
 	&& trace "Selecting Subscription" \
 	&& az account set --subscription $ComponentSubscription \
 	&& echo "$(az account show -o json | jq --raw-output '"\(.name) (\(.id))"')"
